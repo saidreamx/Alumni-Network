aae348f38a04ed06f35069b8f5d0083a17df46b1
getsanjeev <getsanjeevdubey@gmail.com>
2016-10-23 05:31:01 +0530

    signup working

--- app/src/main/java/com/example/sanjeev/alumninetwork/signUp/signUp.java
+++ app/src/main/java/com/example/sanjeev/alumninetwork/signUp/signUp.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.sanjeev.alumninetwork.R;
-import com.example.sanjeev.alumninetwork.profileInfo.MainActivity;
 import com.example.sanjeev.alumninetwork.profileInfo.onePerson;
 
 import java.io.BufferedReader;
@@ -32,13 +31,12 @@ public class signUp extends Fragment
     public static final String ROOT_URL = "http://getsanjeev.esy.es/";
     EditText editTextEmail;
     EditText editTextpassword;
+    EditText cnfmPass;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        Log.e("ZEROTH","ON CREATE VIEW");
-        super.onCreate(savedInstanceState);
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.register, container, false);
-        final Button sign_up = (Button) view.findViewById(R.id.sign_up);
+
+        Button sign_up = (Button) view.findViewById(R.id.sign_up);
         Button login = (Button) view.findViewById(R.id.login_option);
         editTextEmail = (EditText) view.findViewById(R.id.email_edit);
         editTextpassword = (EditText) view.findViewById(R.id.pass_edit);
@@ -55,34 +53,48 @@ public class signUp extends Fragment
             public void onClick(View v)
             {
                 Log.e("FIRST","ENTERED ON CLICK SIGNUP");
-                insertUser();
-                //Intent intent = new Intent(getActivity(), onePerson.class);
-                //startActivity(intent);
+                String email =  editTextEmail.getText().toString();
+                String password = editTextpassword.getText().toString();
+                String cnfm_password = editTextpassword.getText().toString();
+                if((!email.contains("@"))||(email.length()<10))
+                {
+                    Toast.makeText(getContext(), "Invalid Email", Toast.LENGTH_SHORT).show();
+                }
+                if(password.length()<5)
+                {
+                    Toast.makeText(getContext(), "Password Too Short", Toast.LENGTH_SHORT).show();
+                }
+
+                if(!password.equals(cnfm_password))
+                {
+                    Toast.makeText(getContext(), "Password Mismatch", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    insertUser(email, password);
+                    Intent intent = new Intent(getActivity(), onePerson.class);
+                    startActivity(intent);
+                }
             }
         });
         return view;
     }
-    private void insertUser()
+    private void insertUser(String email, String password)
     {
         //Here we will handle the http request to insert user to mysql db
         RestAdapter adapter = new RestAdapter.Builder()
                 .setEndpoint(ROOT_URL) //Setting the Root URL
                 .build();
+
         Log.e("SECOND","ENTERED ON INSERT USER");
         registerAPI api = adapter.create(registerAPI.class);
         api.insertUser(
-
                 //Passing the values by getting it from editTexts
-                editTextpassword.getText().toString(),
-                editTextEmail.getText().toString(),
+               email,password,
                 //Creating an anonymous callback
                 new Callback<Response>() {
                     @Override
                     public void success(Response result, Response response) {
-                        Log.e("THIRD", result.toString());
-                        Log.e("FOURTH",response.toString());
-                        //On success we will read the server's output using bufferedreader
-                        //Creating a bufferedreader object
                         BufferedReader reader = null;
                         //An string to store output from the server
                         String output = "";
@@ -93,15 +105,14 @@ public class signUp extends Fragment
                             output = reader.readLine();
                         } catch (IOException e) {
                             e.printStackTrace();
-                            Log.e("FIFTH",e.toString());
                         }
                         //Displaying the output as a toast
-                        //Toast.makeText(signUp.this, output, Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(), output, Toast.LENGTH_LONG).show();
                     }
 
                     @Override
                     public void failure(RetrofitError error) {
-                        //Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(), error.toString(), Toast.LENGTH_LONG).show();
                         Log.e("SIXTH",error.toString());
                     }
                 }
---
+++ backend/dbConnect.php
@@ -0,0 +1,7 @@
+<?php
+  define('HOST','mysql.hostinger.in');
+  define('USER','u562159323_getu');
+  define('PASS','simplex55');
+  define('DB','u562159323_info');
+  
+  $con = mysqli_connect(HOST,USER,PASS,DB)  or die('Unable to Connect');
