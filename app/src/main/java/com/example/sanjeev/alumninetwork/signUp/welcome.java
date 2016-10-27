63da38390badc1d103028e1ea4ab2e56fe2a7490
getsanjeev <getsanjeevdubey@gmail.com>
2016-10-27 23:47:24 +0530

    added welcome

---
+++ app/src/main/java/com/example/sanjeev/alumninetwork/signUp/welcome.java
@@ -0,0 +1,24 @@
+package com.example.sanjeev.alumninetwork.signUp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sanjeev.alumninetwork.R;
+
+/**
+ * Created by sanjeev on 27/10/16.
+ */
+public class welcome extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.register, container, false);
+
+    }
+}
